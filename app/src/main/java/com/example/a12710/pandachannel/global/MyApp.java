9860ee5999f6448e320f6f6da0cd2669e68a116a
suiyuan <3130773247@qq.com>
2017-07-25 15:01:41 +0800

    2.3.7

--- app/src/main/java/com/example/a12710/pandachannel/global/MyApp.java
+++ app/src/main/java/com/example/a12710/pandachannel/global/MyApp.java
@@ -3,6 +3,8 @@ package com.example.a12710.pandachannel.global;
 import android.app.Application;
 
 import com.example.a12710.pandachannel.base.BaseActivity;
+import com.umeng.message.IUmengRegisterCallback;
+import com.umeng.message.PushAgent;
 import com.umeng.socialize.PlatformConfig;
 import com.umeng.socialize.UMShareAPI;
 
@@ -20,6 +22,20 @@ public class MyApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        PushAgent mPushAgent = PushAgent.getInstance(this);
+//注册推送服务，每次调用register方法都会回调该接口
+        mPushAgent.register(new IUmengRegisterCallback() {
+
+            @Override
+            public void onSuccess(String deviceToken) {
+                //注册成功会返回device token
+            }
+
+            @Override
+            public void onFailure(String s, String s1) {
+
+            }
+        });
         UMShareAPI.get(this);
     }
 
@@ -27,4 +43,5 @@ public class MyApp extends Application {
         PlatformConfig.setWeixin("wx967daebe835fbeac", "5bb696d9ccd75a38c8a0bfe0675559b3");
         PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad", "http://sns.whalecloud.com");
     }
+
 }
