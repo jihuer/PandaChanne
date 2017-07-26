0c94d3a94e3167f26478d5a1eb13376ecb016198
shepixiangjiao <24077247@qq.com>
2017-07-26 10:02:33 +0800

    主页上传

--- app/src/main/java/com/example/a12710/pandachannel/Adapter/YuanChuangAdapter.java
+++ app/src/main/java/com/example/a12710/pandachannel/Adapter/YuanChuangAdapter.java
@@ -8,7 +8,6 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.bumptech.glide.Glide;
 import com.example.a12710.pandachannel.R;
 import com.example.a12710.pandachannel.model.bean.YuanChuangbean;
